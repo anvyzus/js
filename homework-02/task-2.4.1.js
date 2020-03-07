3247885e3fbcf8dca6964f4dba58bee3ca400a0d
Nikita <46813354+greenacny@users.noreply.github.com>
2020-03-07 13:01:22 +0200

    Add HW-02 and -03
    Add homework -02 and -03

---
+++ homework-02/task-2.4.1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let admin;
+let name;
+
+name = 'Джон';
+
+admin = name;
+
+alert(admin);
---
+++ homework-02/task-2.4.3.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const Birthday = '18.04.1982';
+const age = someCode(Birthday);
